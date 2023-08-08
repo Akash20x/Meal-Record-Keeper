351c51e917e7ae3c7ea0d7fe799b67f4fd302d66
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2023-08-08 15:10:11 +0530

    Update adminServices.js

--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_API =  'https://meal-record-api.up.railway.app/api/admin'
+const BASE_API =  'https://meal-record-api.vercel.app/api/admin'
 
 
   export const fetchAllUsers = createAsyncThunk(
