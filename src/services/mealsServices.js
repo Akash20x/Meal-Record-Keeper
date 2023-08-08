02d9969b7dc7076121f1e07610648977a8adf89a
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2023-08-08 15:09:24 +0530

    Update mealsServices.js

--- src/services/mealsServices.js
+++ src/services/mealsServices.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_API =  'https://meal-record-api.up.railway.app/api/meal'
+const BASE_API =  'https://meal-record-api.vercel.app/api/meal'
 
 
 export const createMeal = createAsyncThunk(
